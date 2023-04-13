4b6935b1de4fe1f6b0d5690ee2896fc27cbb5f2a
Colin Osborn <colin.osborn@gmail.com>
2023-04-13 11:02:58 -0600

    updated json objects

--- app.js
+++ app.js
@@ -9,23 +9,28 @@ router.route('/todos')
   .get((req, res) => {
     res.json([
       {
-        "_id": "a",
-        "text": "Item 1",
-        "done": false
-      },
-      {
-        "_id": "b",
-        "text": "Item 2",
-        "done": false
-      },
-      {
-        "_id": "c",
-        "text": "Item 3",
+        "_id": "1",
+        "text": "Put storage wax on Nordic Skis",
         "done": true
       },
       {
-        "_id": "d",
-        "text": "Item 4",
+        "_id": "2",
+        "text": "Wash Blur and install tire inserts",
+        "done": false
+      },
+      {
+        "_id": "3",
+        "text": "Finish fundamental React",
+        "done": true
+      },
+      {
+        "_id": "4",
+        "text": "Read The Road to React",
+        "done": false
+      },
+      {
+        "_id": "5",
+        "text": "Complete level 8 code wars challengers",
         "done": false
       },
     ]);
